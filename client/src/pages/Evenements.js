e3d328bc701c08d499e9662a2c3be013abac6957
ignaesLionel <ignaes_lionel@hotmail.com>
2021-02-21 20:43:09 +0100

    suite

--- client/src/pages/Evenements.js
+++ client/src/pages/Evenements.js
@@ -5,7 +5,7 @@ const Evenements = () => {
     return (
         <div>
             <Navigation/>
-            <h1> Nos événements </h1>
+            <h1> Nos évènements </h1>
         </div>
     );
 };
