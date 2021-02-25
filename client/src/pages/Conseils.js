ef022aed2d928d8de580b674b5ad5368777cae2f
ignaesLionel <ignaes_lionel@hotmail.com>
2021-02-25 22:23:30 +0100

    modif couleur

--- client/src/pages/Conseils.js
+++ client/src/pages/Conseils.js
@@ -7,15 +7,14 @@ const Conseils = () => {
             <Navigation/>
             <h1 className="h1-1">Nos Conseils</h1>
 
-            <h2 className="h2-1">Conseils avant adoption</h2>
-            <h2 className="h2-1">Ce que dit la loi</h2>
-
-L’article L211-27 du Code rural et de la Pêche maritime prévoit des règles pour formaliser la bonne gestion des populations de chats errants. Le maire peut autoriser leur capture en vue de leur stérilisation et leur identification, puis leur réintégration sur leur lieu de vie.
-            <h2 className="h2-1">La stérélisation des chats</h2>
-            <h2 className="h2-1">L'identification</h2>
-            <h2 className="h2-1">L'adoption d'un chat</h2>
-            <h2 className="h2-1">Bricolage</h2>
-            <h2 className="h2-1">Trucs et astuces</h2>
+            <h2 className="h2-1">Conseils avant adoption</h2><br />
+            <h2 className="h2-1">Ce que dit la loi</h2><br />
+L’article L211-27 du Code rural et de la Pêche maritime prévoit des règles pour formaliser la bonne gestion des populations de chats errants. Le maire peut autoriser leur capture en vue de leur stérilisation et leur identification, puis leur réintégration sur leur lieu de vie.<br />
+            <h2 className="h2-1">La stérélisation des chats</h2><br />
+            <h2 className="h2-1">L'identification</h2><br />
+            <h2 className="h2-1">L'adoption d'un chat</h2><br />
+            <h2 className="h2-1">Bricolage</h2><br />
+            <h2 className="h2-1">Trucs et astuces</h2><br />
         </div>
     );
 };
