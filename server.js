d4266e4c65caa6b05478630919e4fd8b3868d0b2
ignaesLionel <ignaes_lionel@hotmail.com>
2021-03-09 21:39:11 +0100

    1

--- server.js
+++ server.js
@@ -35,6 +35,10 @@ app.use(bodyParser.urlencoded({
 // routes
 app.use('/api/user',userRoutes);
 
+app.get("/checkthis", function(req, res, next) {
+  res.send("Yes");
+});
+
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
