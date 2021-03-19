70eef9d36bb416ece29abce8d4efe54a41f8ea70
ignaesLionel <ignaes_lionel@hotmail.com>
2021-03-19 08:54:45 +0100

    modify folder

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/',fileRoutes);
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
-    
+    console.log("production")
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
