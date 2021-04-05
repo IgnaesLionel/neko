66cfc847f5d8de8ee715dfefae04a8722c75c9c3
ignaesLionel <ignaes_lionel@hotmail.com>
2021-04-05 20:16:10 +0200

    .

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -75,7 +75,6 @@ const deleteFile = (req, res) => {
   const fileName = req.params.id;
   const directoryPath = __basedir + "/resources/uploads/";
   const targetfile = `${directoryPath}${fileName}`
-console.log(targetfile)
   fs.unlink(targetfile, (err) => {
     res.send({
       status: "200",
