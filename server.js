57f3fffd397cc99df5b46e57f27edba57ea2ad9a
ignaesLionel <ignaes_lionel@hotmail.com>
2021-05-08 08:23:27 +0200

    .

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cors({
     return callback(null, true);
   },credentials: true,
   'allowedHeaders': ['sessionId', 'Content-Type', 'Authorization'],
-  'exposedHeaders': ['sessionId'],
+  'exposedHeaders': ['sessionId', 'Authorization'],
   'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
   'preflightContinue': false
 
